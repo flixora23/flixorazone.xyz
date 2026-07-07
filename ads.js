02bc818291df1514cf40ff2036ca08aa74e20f90
flixora23 <suryaseptian244@gmail.com>
2026-07-07 18:35:31 +0700

    Code modification ads.js via MD Zone

--- ads.js
+++ ads.js
@@ -6,52 +6,52 @@
 // 1. Iklan Popunder
 // ===============================
 function loadPopunder() {
-    document.write('<script src="https://pl30241638.effectivecpmnetwork.com/3a/7a/10/3a7a100807ad7d3d7d767ce7965da031.js"><\/script>');
+    document.write('<script src="https://pl30244556.effectivecpmnetwork.com/af/3b/5f/af3b5f73cbc9f133c472e8fe20cd6d4f.js"><\/script>');
 }
 
 // ===============================
 // 2. Iklan Native Banner
 // ===============================
 function loadNativeAd() {
-    document.write('<script async="async" data-cfasync="false" src="https://pl30241640.effectivecpmnetwork.com/7fe94f462533c1cd2a0c1d9721c66f1d/invoke.js"><\/script>');
-    document.write('<div id="container-7fe94f462533c1cd2a0c1d9721c66f1d"></div>');
+    document.write('<script async="async" data-cfasync="false" src="https://pl30244557.effectivecpmnetwork.com/82e68430052f8a79d6ccc6604ddd747f/invoke.js"><\/script>');
+    document.write('<div id="container-82e68430052f8a79d6ccc6604ddd747f"></div>');
 }
 
 // ===============================
 // 3. Iklan Social Bar
 // ===============================
 function loadSocialBar() {
-    document.write('<script src="https://pl30241639.effectivecpmnetwork.com/9a/ee/0b/9aee0bc1999a8c3261aeb9327cf826be.js"><\/script>');
+    document.write('<script src="https://pl30244559.effectivecpmnetwork.com/f6/b7/5e/f6b75e7df40a7f91f8a6c94bfeea03e6.js"><\/script>');
 }
 
 // ===============================
 // 4. Iklan Banner 468x60
 // ===============================
 function loadBanner468x60() {
-    document.write('<script>atOptions = { "key" : "3d9a19ac08e88cd17fc603f400f4d53e", "format" : "iframe", "height" : 60, "width" : 468, "params" : {} };<\/script>');
-    document.write('<script src="https://www.highperformanceformat.com/3d9a19ac08e88cd17fc603f400f4d53e/invoke.js"><\/script>');
+    document.write('<script>atOptions = { "key" : "7ca4fe74dad01c1fced4997dca958f8c", "format" : "iframe", "height" : 60, "width" : 468, "params" : {} };<\/script>');
+    document.write('<script src="https://www.highperformanceformat.com/7ca4fe74dad01c1fced4997dca958f8c/invoke.js"><\/script>');
 }
 
 // ===============================
 // 5. Iklan Banner 300x250
 // ===============================
 function loadBanner300x250() {
-    document.write('<script>atOptions = { "key" : "a01b26364b3fdb874607d1ae2a8c42ab", "format" : "iframe", "height" : 250, "width" : 300, "params" : {} };<\/script>');
-    document.write('<script src="https://www.highperformanceformat.com/a01b26364b3fdb874607d1ae2a8c42ab/invoke.js"><\/script>');
+    document.write('<script>atOptions = { "key" : "64169d6e4c9f9794f2e0945670f4f159", "format" : "iframe", "height" : 250, "width" : 300, "params" : {} };<\/script>');
+    document.write('<script src="https://www.highperformanceformat.com/64169d6e4c9f9794f2e0945670f4f159/invoke.js"><\/script>');
 }
 
 // ===============================
 // 6. Iklan Banner 728x90
 // ===============================
 function loadBanner728x90() {
-    document.write('<script>atOptions = { "key" : "ddfd85f67415dc4735ec41a18ffe256f", "format" : "iframe", "height" : 90, "width" : 728, "params" : {} };<\/script>');
-    document.write('<script src="https://www.highperformanceformat.com/ddfd85f67415dc4735ec41a18ffe256f/invoke.js"><\/script>');
+    document.write('<script>atOptions = { "key" : "942621e6aa931d279e92769ed0ff83aa", "format" : "iframe", "height" : 90, "width" : 728, "params" : {} };<\/script>');
+    document.write('<script src="https://www.highperformanceformat.com/942621e6aa931d279e92769ed0ff83aa/invoke.js"><\/script>');
 }
 
 // ===============================
 // 7. Iklan Banner 320x50
 // ===============================
 function loadBanner320x50() {
-    document.write('<script>atOptions = { "key" : "f4d45eaf45f4d5b7dec808b63e0ee782", "format" : "iframe", "height" : 50, "width" : 320, "params" : {} };<\/script>');
-    document.write('<script src="https://www.highperformanceformat.com/f4d45eaf45f4d5b7dec808b63e0ee782/invoke.js"><\/script>');
+    document.write('<script>atOptions = { "key" : "41cfb5e0cd602cb26edd98c2718ee0bf", "format" : "iframe", "height" : 50, "width" : 320, "params" : {} };<\/script>');
+    document.write('<script src="https://www.highperformanceformat.com/41cfb5e0cd602cb26edd98c2718ee0bf/invoke.js"><\/script>');
 }
